2ce0102448d2fba994871f02e4a03a8577d28b1d
Gergoa123 <gergo3124@gmail.com>
2022-06-09 16:45:25 +0300

    Add project files.

---
+++ Tema5Ex2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tema5Ex2
+{
+    // Ex.2 Scrieti un program care sa verifice daca un sir de caractere citit de la
+    //      tastatura este sau nu palindrom.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti stringul!");
+            string cuvant = Console.ReadLine();
+            string invers = "" ;
+
+
+            for (int i = cuvant.Length - 1; i >= 0; i--)
+            {
+                
+                 invers += cuvant[i].ToString();
+            }
+
+            Console.WriteLine(invers);
+        }
+    }
+}
